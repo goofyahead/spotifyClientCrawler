33f598e13da47df1b1360646497c9bc32b79b693
Siwei Yang <yangsiwei880813@gmail.com>
2013-04-24 15:44:57 -0700

    store id and set as processed

--- idCrawler/lib/SpotifyIdScrapper.js
+++ idCrawler/lib/SpotifyIdScrapper.js
@@ -15,20 +15,19 @@ exports.SpotifyIdCrawler = function SpotifyIdCrawler() {
 
         var url = 'http://open.spotify.com/user/' + item;
         console.log(url.toString().green);
-        api.storeData(item, null, callback);
-        // api.loadUrl(url, {}, _x(callback, true, function(error, body, res) {
 
-        //     $ = cheerio.load(body);
+        api.loadUrl(url, {}, _x(callback, true, function(error, body, res) {
 
-        //     if ($('meta[property="fb:profile_id"]').length > 0) {
-        //         var fb_id = $('meta[property="fb:profile_id"]').attr('content');
-        //         console.log(fb_id.toString().yellow + ' found'.yellow + ' to spotify id: '.yellow + item.toString().yellow);
+            $ = cheerio.load(body);
 
-        //         api.engine.options.outIds.store(item.toString(), {'fb_id' : fb_id}, _x(callback, true, function(err) {
-        //             callback(null);
-        //         }));
-        //     }
+            if ($('meta[property="fb:profile_id"]').length > 0) {
+                var fb_id = $('meta[property="fb:profile_id"]').attr('content');
+                console.log(fb_id.toString().yellow + ' found'.yellow + ' to spotify id: '.yellow + item.toString().yellow);
 
-        // }));
+                api.engine.options.outIds.store(item.toString(), {'fb_id' : fb_id}, function () {});
+
+                api.storeData(item, null, callback);
+            }
+        }));
     };
 };
