775bea837b1db6108e4bdb039dd4646c22520428
Alex Vidal <goofyahead@gmail.com>
2013-05-12 09:28:50 +0200

    store fb id and music

--- musicListCrawler/lib/SpotifyMusicListCrawler.js
+++ musicListCrawler/lib/SpotifyMusicListCrawler.js
@@ -4,20 +4,37 @@ var _e = laeh2._e;
 
 var _ = require('underscore');
 var colors = require('colors');
+var cheerio = require('cheerio');
 
 exports.SpotifyMusicListCrawler = function SpotifyMusicListCrawler() {
 
     this.process = function( item, data, api, callback) {
-        api.logger.info(item);
-        var that = this;
-        var data = {};
+      var data = {};
 
-        var url = 'http://immense-stream-2556.herokuapp.com/spotify/'
-        + item +'?key=arkguinscanfly2';
-        console.log('call for ' + item);
-        api.loadUrl(url, {}, _x(callback, true, function(error, body, res) {
-          console.log(body);
-          api.storeData(item, body, _x(callback, true, callback));
-        }));
+      var urlSpoty = 'http://open.spotify.com/user/' + item;
+        console.log(urlSpoty.toString().green);
+
+        api.loadUrl(urlSpoty, {}, function(error, body, res) {
+
+            $ = cheerio.load(body);
+
+            if ($('meta[property="fb:profile_id"]').length > 0) {
+                var fb_id = $('meta[property="fb:profile_id"]').attr('content');
+                console.log(fb_id.toString().yellow + ' found'.yellow + ' to spotify id: '.yellow + item.toString().yellow);
+
+                data['fb_id'] = fb_id;
+                api.logger.info(item);
+                var that = this;
+
+                var url = 'http://immense-stream-2556.herokuapp.com/spotify/'
+                + item +'?key=arkguinscanfly2';
+                //console.log('call for ' + item);
+                api.loadUrl(url, {}, function(error, body, res) {
+                  console.log(body);
+                  data['music'] = JSON.parse(body);
+                  api.storeData(item, data, _x(callback, true, callback));
+                });
+            }
+        });      
     };
 };
